7d446d4f57c6c96058e58f91ff9f5a4ede65c570
Tara Gibson <tara@pnnl.gov>
2014-07-08 14:08:38 -0700

    Added phase angle difference data class

---
+++ pmu-core/src/main/java/pnnl/goss/tutorial/datamodel/PMUPhaseAngleDiffData.java
@@ -0,0 +1,41 @@
+package pnnl.goss.tutorial.datamodel;
+
+import java.util.Date;
+
+public class PMUPhaseAngleDiffData {
+	private Date timestamp;
+	private Double phasor1;
+	private Double phasor2;
+	private Double difference;
+	
+	
+	
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Double getPhasor1() {
+		return phasor1;
+	}
+	public void setPhasor1(Double phasor1) {
+		this.phasor1 = phasor1;
+	}
+	public Double getPhasor2() {
+		return phasor2;
+	}
+	public void setPhasor2(Double phasor2) {
+		this.phasor2 = phasor2;
+	}
+	public Double getDifference() {
+		return difference;
+	}
+	public void setDifference(Double difference) {
+		this.difference = difference;
+	}
+	
+	
+	
+}
