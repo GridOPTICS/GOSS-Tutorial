a3078ebaff339d668d5f80278960bb7f7317d1c3
Poorva Sharma <poorva.sharma@pnnl.gov>
2014-07-10 14:27:59 -0700

    implements serializable

--- pmu-core/src/main/java/pnnl/goss/tutorial/datamodel/PMUPhaseAngleDiffData.java
+++ pmu-core/src/main/java/pnnl/goss/tutorial/datamodel/PMUPhaseAngleDiffData.java
@@ -1,8 +1,11 @@
 package pnnl.goss.tutorial.datamodel;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class PMUPhaseAngleDiffData {
+public class PMUPhaseAngleDiffData implements Serializable {
+	
+	private static final long serialVersionUID = 3219362975964656381L;
 	private Date timestamp;
 	private Double phasor1;
 	private Double phasor2;
