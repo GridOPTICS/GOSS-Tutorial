7e67da7a161d2766b30e188c3a12ab0e20cd1848
Craig Allwardt <craig.allwardt@pnnl.gov>
2014-07-22 09:01:52 -0700

    Modified launcher to include bundle starting mechanism.

--- pmu-common/src/main/java/pnnl/goss/tutorial/launchers/PythonJavaGatewayLauncher.java
+++ pmu-common/src/main/java/pnnl/goss/tutorial/launchers/PythonJavaGatewayLauncher.java
@@ -1,13 +1,24 @@
 package pnnl.goss.tutorial.launchers;
 
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Validate;
+
 import py4j.GatewayServer;
 
 //This is needed so that py4j can make java calls
+@Component
+@Instantiate
 public class PythonJavaGatewayLauncher extends Thread{
 
+	@Validate
+	public void startLauncher(){
+		System.out.println("Starting Pythong gateway.");
+		new PythonJavaGatewayLauncher().start();
+	}
 	
 	public static void main(String[] args) {
-		new PythonJavaGatewayLauncher().start();
+		new PythonJavaGatewayLauncher().startLauncher();
 	}
 
 	@Override
