1f6a48aeeba7a12e07a9f20530c03755a48ac4a8
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-01-08 13:25:52 -0800

    Removed spaces.

--- goss-tutorial-common/src/main/java/pnnl/goss/tutorial/request/TutorialDownloadRequestAsync.java
+++ goss-tutorial-common/src/main/java/pnnl/goss/tutorial/request/TutorialDownloadRequestAsync.java
@@ -4,62 +4,62 @@ import java.util.Date;
 
 import pnnl.goss.core.RequestAsync;
 
-public class TutorialDownloadRequestAsync extends RequestAsync {
-	
-	Date startDate;
-	long startTime;
-	long endTime;
-	int count;
-	int segment;
-	
-	public TutorialDownloadRequestAsync(long startTime, long endTime, int segment, Date startDate){
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.segment = segment;
-		this.startDate = startDate;
-	}
+public class TutorialDownloadRequestAsync extends  RequestAsync {
 
-	public Date getStartDate() {
-		return startDate;
-	}
+    Date startDate;
+    long startTime;
+    long endTime;
+    int count;
+    int segment;
 
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
+    public TutorialDownloadRequestAsync(long startTime, long endTime, int segment, Date startDate){
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.segment = segment;
+        this.startDate = startDate;
+    }
 
-	public long getStartTime() {
-		return startTime;
-	}
+    public Date getStartDate() {
+        return startDate;
+    }
 
-	public void setStartTime(long startTime) {
-		this.startTime = startTime;
-	}
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
 
-	public long getEndTime() {
-		return endTime;
-	}
+    public long getStartTime() {
+        return startTime;
+    }
 
-	public void setEndTime(long endTime) {
-		this.endTime = endTime;
-	}
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
 
-	public int getCount() {
-		return count;
-	}
+    public long getEndTime() {
+        return endTime;
+    }
 
-	public void setCount(int count) {
-		this.count = count;
-	}
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
 
-	public int getSegment() {
-		return segment;
-	}
+    public int getCount() {
+        return count;
+    }
 
-	public void setSegment(int segment) {
-		this.segment = segment;
-	}
-	
-	
-	
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getSegment() {
+        return segment;
+    }
+
+    public void setSegment(int segment) {
+        this.segment = segment;
+    }
+
+
+
 
 }
