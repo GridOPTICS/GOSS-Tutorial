6af75ca4d22d51ce22fcb7b5c7042d93cbf45f41
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-01-08 13:27:21 -0800

    Updated fakeclient to inherit from client.

--- goss-tutorial-server/src/test/java/pnnl/goss/tutorial/FakeClient.java
+++ goss-tutorial-server/src/test/java/pnnl/goss/tutorial/FakeClient.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import javax.jms.IllegalStateException;
 import javax.jms.JMSException;
 
-import pnnl.goss.core.Data;
 import pnnl.goss.core.DataResponse;
 import pnnl.goss.core.Request;
 import pnnl.goss.core.Request.RESPONSE_FORMAT;
@@ -15,52 +14,52 @@ import pnnl.goss.core.client.GossResponseEvent;
 
 public class FakeClient implements Client {
 
-	// Handlings passing data from/to topic
-	HashMap<String, Serializable> pubSubResponeCache = new HashMap<String,Serializable>();
-	// Handles the subscribing of an event.
-	HashMap<String, GossResponseEvent> topicEvent = new HashMap<String, GossResponseEvent>();
+    // Handlings passing data from/to topic
+    HashMap<String, Serializable> pubSubResponeCache = new HashMap<String,Serializable>();
+    // Handles the subscribing of an event.
+    HashMap<String, GossResponseEvent> topicEvent = new HashMap<String, GossResponseEvent>();
 
-	
-	
-	public boolean isSubscribed(String topic){
-		return topicEvent.containsKey(topic);
-	}
-	
-	@Override
-	public Object getResponse(Request request) throws IllegalStateException,
-			JMSException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public Object getResponse(Request request, RESPONSE_FORMAT responseFormat) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public String sendRequest(Request request, GossResponseEvent event,
-			RESPONSE_FORMAT responseFormat) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
-		
+    public boolean isSubscribed(String topic){
+        return topicEvent.containsKey(topic);
+    }
 
-	}
+    @Override
+    public Object getResponse(Request request) throws IllegalStateException,
+            JMSException {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void subscribeTo(String topicName, GossResponseEvent event)
-			throws NullPointerException {
-		// TODO Auto-generated method stub
-		topicEvent.put(topicName, event);
+    @Override
+    public Object getResponse(Request request, RESPONSE_FORMAT responseFormat) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	}
+    @Override
+    public String sendRequest(Request request, GossResponseEvent event,
+            RESPONSE_FORMAT responseFormat) throws NullPointerException {
+        // TODO Auto-generated method stub
+        return null;
+
+
+    }
+
+    @Override
+    public void subscribeTo(String topicName, GossResponseEvent event)
+            throws NullPointerException {
+        // TODO Auto-generated method stub
+        topicEvent.put(topicName, event);
+
+    }
 
 //	@Override
 //	public void publish(String topicName, Serializable data,
 //			RESPONSE_FORMAT responseFormat) throws NullPointerException {
-//		
-//		
+//
+//
 //
 //	}
 
@@ -73,48 +72,48 @@ public class FakeClient implements Client {
 //		}
 //	}
 
-	@Override
-	public void close() {
-		// TODO Auto-generated method stub
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void publish(String topicName, Serializable data,
-			RESPONSE_FORMAT responseFormat) throws NullPointerException {
-		pubSubResponeCache.put(topicName, data);
-		if (isSubscribed(topicName)){
-			topicEvent.get(topicName).onMessage(new DataResponse(data));
-		}
-		
-	}
+    @Override
+    public void publish(String topicName, Serializable data,
+            RESPONSE_FORMAT responseFormat) throws NullPointerException {
+        pubSubResponeCache.put(topicName, data);
+        if (isSubscribed(topicName)){
+            topicEvent.get(topicName).onMessage(new DataResponse(data));
+        }
 
-	@Override
-	public void publish(String topicName, Serializable data)
-			throws NullPointerException {
-		pubSubResponeCache.put(topicName, data);
-		if (isSubscribed(topicName)){
-			topicEvent.get(topicName).onMessage(new DataResponse(data));
-		}
-		
-	}
+    }
 
-	@Override
-	public void publish(String topicName, Data data,
-			RESPONSE_FORMAT responseFormat) throws NullPointerException {
-		pubSubResponeCache.put(topicName, data);
-		if (isSubscribed(topicName)){
-			topicEvent.get(topicName).onMessage(new DataResponse(data));
-		}
-	}
+    @Override
+    public void publish(String topicName, Serializable data)
+            throws NullPointerException {
+        pubSubResponeCache.put(topicName, data);
+        if (isSubscribed(topicName)){
+            topicEvent.get(topicName).onMessage(new DataResponse(data));
+        }
 
-	@Override
-	public void publishString(String topicName, String data
-			) throws NullPointerException {
-		pubSubResponeCache.put(topicName, data);
-		if (isSubscribed(topicName)){
-			topicEvent.get(topicName).onMessage(new DataResponse(data));
-		}
-		
-	}
+    }
+
+//    @Override
+//    public void publish(String topicName, Data data,
+//            RESPONSE_FORMAT responseFormat) throws NullPointerException {
+//        pubSubResponeCache.put(topicName, data);
+//        if (isSubscribed(topicName)){
+//            topicEvent.get(topicName).onMessage(new DataResponse(data));
+//        }
+//    }
+
+    @Override
+    public void publishString(String topicName, String data
+            ) throws NullPointerException {
+        pubSubResponeCache.put(topicName, data);
+        if (isSubscribed(topicName)){
+            topicEvent.get(topicName).onMessage(new DataResponse(data));
+        }
+
+    }
 }
