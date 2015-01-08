fc6bdaffb0964a82fb66f3546db88e8ab156ff7c
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-01-08 13:26:24 -0800

    Fixed spacing added abstractRequestHandler

--- goss-tutorial-server/src/main/java/pnnl/goss/tutorial/handlers/TutorialDesktopHandler.java
+++ goss-tutorial-server/src/main/java/pnnl/goss/tutorial/handlers/TutorialDesktopHandler.java
@@ -7,43 +7,43 @@ import java.text.SimpleDateFormat;
 import pnnl.goss.core.Request;
 import pnnl.goss.core.UploadRequest;
 import pnnl.goss.core.UploadResponse;
-import pnnl.goss.server.core.GossRequestHandler;
+import pnnl.goss.core.server.AbstractRequestHandler;
 import pnnl.goss.tutorial.datamodel.PMUPhaseAngleDiffData;
-import static pnnl.goss.tutorial.launchers.PMUTutorialActivator.PROP_TUTORIALDB_DATASERVICE;
-public class TutorialDesktopHandler extends GossRequestHandler{
-	
+// import static pnnl.goss.tutorial.launchers.PMUTutorialActivator.PROP_TUTORIALDB_DATASERVICE;
+public class TutorialDesktopHandler extends AbstractRequestHandler{
+
 public UploadResponse handle(Request request) {
-		
-		UploadResponse response = null;
-		
-		try{
-			UploadRequest uploadrequest = (UploadRequest)request;
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-			
-			Connection connection = this.dataservices.getPooledConnection(PROP_TUTORIALDB_DATASERVICE);
+
+        UploadResponse response = null;
+
+        try{
+            UploadRequest uploadrequest = (UploadRequest)request;
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+            Connection connection = null; //this.dataservices.getPooledConnection(PROP_TUTORIALDB_DATASERVICE);
 //			Connection connection = GOSSTutorialDataSource.getInstance().getConnection();
 //			System.out.println(connection);
-			Statement statement = connection.createStatement();
-			
-			PMUPhaseAngleDiffData data = (PMUPhaseAngleDiffData)uploadrequest.getData();
-			
-			String time = formatter.format(data.getTimestamp());
-			String queryString = "replace into aggregator(`time`,phasor1,phasor2,diff) values "+
-									"('"+time+"',"+data.getPhasor1()+","+data.getPhasor2()+","+data.getDifference()+")";
-			System.out.println(queryString);
-			int rows =  statement.executeUpdate(queryString);
-			System.out.println(rows);
-			//connection.commit();
-			connection.close();
-		}
-		catch(Exception e){
-			response = new UploadResponse(false);
-			response.setMessage(e.getMessage());
-			e.printStackTrace();
-			return response;
-		}
-		response = new UploadResponse(true);
-		return response;
-	}
+            Statement statement = connection.createStatement();
+
+            PMUPhaseAngleDiffData data = (PMUPhaseAngleDiffData)uploadrequest.getData();
+
+            String time = formatter.format(data.getTimestamp());
+            String queryString = "replace into aggregator(`time`,phasor1,phasor2,diff) values "+
+                                    "('"+time+"',"+data.getPhasor1()+","+data.getPhasor2()+","+data.getDifference()+")";
+            System.out.println(queryString);
+            int rows =  statement.executeUpdate(queryString);
+            System.out.println(rows);
+            //connection.commit();
+            connection.close();
+        }
+        catch(Exception e){
+            response = new UploadResponse(false);
+            response.setMessage(e.getMessage());
+            e.printStackTrace();
+            return response;
+        }
+        response = new UploadResponse(true);
+        return response;
+    }
 
 }
