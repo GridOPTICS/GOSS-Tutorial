bd89b8a7a271b5c9eaa66bfb37abc9cf22decd99
Tara Gibson <tara@pnnl.gov>
2015-01-13 14:27:40 -0800

    Cleanup resource http service on unactivate

--- goss-tutorial-web-ui/src/main/java/pnnl/goss/tutorial/web/TutorialWebActivator.java
+++ goss-tutorial-web-ui/src/main/java/pnnl/goss/tutorial/web/TutorialWebActivator.java
@@ -11,30 +11,28 @@ import org.ops4j.pax.web.extender.whiteboard.ResourceMapping;
 import org.ops4j.pax.web.extender.whiteboard.runtime.DefaultResourceMapping;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
 
 public class TutorialWebActivator implements BundleActivator{
     private static Logger log = LoggerFactory.getLogger(TutorialWebActivator.class);
-    
+    ServiceRegistration resourceService;
 	@Override
 	public void start(BundleContext context) throws Exception {
         log.debug(" Bundle starting! "+getClass());
-//        Dictionary props = new Hashtable();
-//        props.put( "alias", "/tutorialServlet" );
-//        props.put("servlet-name", "Tutorial Servlet");
-//        context.registerService(Servlet.class.getName(), new TutorialServlet(), props);
 		
         DefaultResourceMapping resourceMapping = new DefaultResourceMapping();
         resourceMapping.setAlias( "/tutorial" );
         resourceMapping.setPath( "/" );
-        context.registerService( ResourceMapping.class.getName(), resourceMapping, null );
+        resourceService = context.registerService( ResourceMapping.class.getName(), resourceMapping, null );
 
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-        log.debug("Bundle starting! "+getClass());
-
-		
+        log.debug("Bundle stopping! "+getClass());
+		if(resourceService!=null){
+			resourceService.unregister();
+		}
 	}
 
 }
