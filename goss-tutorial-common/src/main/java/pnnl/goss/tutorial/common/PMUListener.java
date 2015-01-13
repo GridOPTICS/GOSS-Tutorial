79baf4ff8c9e04e1ae839fe5e1a442136e90ca8d
Craig Allwardt <craig.allwardt@pnnl.gov>
2015-01-13 08:48:58 -0800

    Removed unused class.

--- goss-tutorial-common/src/main/java/pnnl/goss/tutorial/common/PMUListener.java
+++
@@ -1,5 +0,0 @@
-package pnnl.goss.tutorial;
-
-public interface PMUListener {
-
-}
--- goss-tutorial-server/src/main/java/pnnl/goss/tutorial/impl/PMUListenerImpl.java
+++
@@ -1,7 +0,0 @@
-package pnnl.goss.tutorial.impl;
-
-import pnnl.goss.tutorial.PMUListener;
-
-public class PMUListenerImpl implements PMUListener{
-
-}
